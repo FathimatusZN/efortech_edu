6598fc1c304227e620febcbe29391f2961657aed
unknown <tafdhilaanadya@gmail.com>
2025-03-18 09:54:00 +0700

    feat(auth): add change-password page, update signin and register page

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -4,16 +4,39 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-const SigninPage = () => {
+const RegisterPage = () => {
     const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false); 
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
 
-    const togglePasswordVisibility = () => {
-        setShowPassword((prev) => !prev);
+    const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+    const toggleConfirmPasswordVisibility = () => setShowConfirmPassword((prev) => !prev); 
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setError("");
+
+        if (!email.includes("@")) {
+            setError("");
+            return;
+        }
+
+        if (password !== confirmPassword) {
+            setError("Password and Confirm Password do not match.")
+            return;
+        }
+
+        alert("Registrasi berhasil!");
     };
 
     return (
-        <div className="w-full h-screen flex flex-col md:flex-row -mt-16">
-            <div className="relative w-full md:w-1/2 h-64 md:h-full overflow-hidden">
+        <div className="w-full min-h-screen flex flex-col md:flex-row -mt-16">
+
+            <div className="relative w-full md:w-1/2 h-auto md:h-auto overflow-hidden">
                 <img
                     src="/assets/Gambar2.jpg"
                     alt="Register Image"
@@ -21,94 +44,108 @@ const SigninPage = () => {
                 />
             </div>
 
-            <div className="w-full md:w-1/2 h-full flex items-center justify-center p-4">
-                <div className="w-full max-w-md space-y-6 mt-[-60]">
+            <div className="w-full md:w-1/2 flex items-center justify-center p-6">
+                <div className="w-[550px] h-[650px] space-y-6 flex flex-col justify-center">
                     <h1 className="text-3xl font-bold text-[#333333] text-center">Register Form</h1>
 
-                    <form className="border-2 border-[#03649F] rounded-[10px] p-6 space-y-5 fi bg-white ml-[-50] w-[550px] h-[420px] shadow-md">
-
-                        <div className="space-y-2">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="border-2 border-[#03649F] rounded-[10px] p-6 space-y-5 bg-white shadow-md h-full flex flex-col justify-between"
+                    >
+                        <div className="space-y-4">
+ 
+                            <div className="space-y-2">
                                 <label className="text-lg font-semibold text-[#333333] flex items-center">
                                     Full Name <span className="text-red-500 ml-1">*</span>
                                 </label>
                                 <input
-                                    type="name"
+                                    type="text"
                                     placeholder="Enter your name here"
-                                    className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] shadow-md"
+                                    className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] shadow-md"
+                                    required
                                 />
                             </div>
 
-                        <div className="space-y-2">
-                            <label className="text-lg font-semibold text-[#333333] flex items-center">
-                                Email <span className="text-red-500 ml-1">*</span>
-                            </label>
-                            <input
-                                type="email"
-                                placeholder="Enter your email in the correct format"
-                                className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] shadow-md"
-                            />
-                        </div>
-
-                        <div className="space-y-2">
-                            <label className="text-lg font-semibold text-[#333333] flex items-center">
-                                Password <span className="text-red-500 ml-1">*</span>
-                            </label>
-                            <div className="relative">
+                            <div className="space-y-2">
+                                <label className="text-lg font-semibold text-[#333333] flex items-center">
+                                    Email <span className="text-red-500 ml-1">*</span>
+                                </label>
                                 <input
-                                    type={showPassword ? "text" : "password"}
-                                    placeholder="Enter your password here"
-                                    className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] pr-12 shadow-md"
+                                    type="email"
+                                    placeholder="Enter your email"
+                                    className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] shadow-md"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
                                 />
-                                <div
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
-                                >
-                                    {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
+                            </div>
+
+                            <div className="space-y-2">
+                                <label className="text-lg font-semibold text-[#333333] flex items-center">
+                                    Password <span className="text-red-500 ml-1">*</span>
+                                </label>
+                                <div className="relative">
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder="Enter your password"
+                                        className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] pr-12 shadow-md"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                    <div
+                                        onClick={togglePasswordVisibility}
+                                        className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
+                                    >
+                                        {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className="space-y-2">
-                            <label className="text-lg font-semibold text-[#333333] flex items-center">
-                                Confirm Password <span className="text-red-500 ml-1">*</span>
-                            </label>
-                            <div className="relative">
-                                <input
-                                    type={showPassword ? "text" : "password"}
-                                    placeholder="Enter your password"
-                                    className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] pr-12 shadow-md"
-                                />
-                                <div
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
-                                >
-                                    {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
+                            <div className="space-y-2">
+                                <label className="text-lg font-semibold text-[#333333] flex items-center">
+                                    Confirm Password <span className="text-red-500 ml-1">*</span>
+                                </label>
+                                <div className="relative">
+                                    <input
+                                        type={showConfirmPassword ? "text" : "password"}
+                                        placeholder="Confirm your password"
+                                        className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] pr-12 shadow-md"
+                                        value={confirmPassword}
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                        required
+                                    />
+                                    <div
+                                        onClick={toggleConfirmPasswordVisibility}
+                                        className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
+                                    >
+                                        {showConfirmPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
+                                    </div>
                                 </div>
                             </div>
+
+                            {error && (
+                                <p className="text-red-600 text-sm">
+                                    {error}
+                                </p>
+                            )}
+
+                            <div className="flex justify-center pt-16">
+                                <button
+                                    type="submit"
+                                    className="bg-[#ED7117] hover:bg-orange-600 text-white font-semibold rounded-[10px] w-[180px] h-[40px] transition"
+                                >
+                                    Register
+                                </button>
+                            </div>
+
+                            <p className="text-gray-500 text-center text-sm">
+                                Already have an account?{" "}
+                                <Link href="../auth/signin" className="text-[#ED7117] font-semibold hover:underline">
+                                    Sign In
+                                </Link>
+                            </p>
                         </div>
-
-
-                        <div className="flex justify-end">
-                            <Link href="/change-password" className="text-[#ED7117] text-sm font-semibold hover:underline">
-                                Forgot Password?
-                            </Link>
-                        </div>
-
-                        <div className="flex justify-center pt-12">
-                            <button
-                                type="submit"
-                                className="bg-[#ED7117] hover:bg-orange-600 text-white font-semibold rounded-[10px] w-[180px] h-[36px] transition"
-                            >
-                                Sign In
-                            </button>
-                        </div>
-
-                        <p className="text-gray-500 text-center text-sm">
-                            Don't have an account?{" "}
-                            <Link href="/signin" className="text-[#ED7117] font-semibold hover:underline">
-                                Register
-                            </Link>
-                        </p>
                     </form>
                 </div>
             </div>
@@ -116,4 +153,4 @@ const SigninPage = () => {
     );
 };
 
-export default SigninPage;
+export default RegisterPage;
--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -6,14 +6,50 @@ import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SigninPage = () => {
     const [showPassword, setShowPassword] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
 
     const togglePasswordVisibility = () => {
         setShowPassword((prev) => !prev);
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setEmailError("");
+        setPasswordError("");
+
+        let valid = true;
+
+        if (!email) {
+            setEmailError("Email cannot be empty.");
+            valid = false;
+        } else if (email !== "admin@example.com") {
+            setEmailError("Email is not registered.");
+            valid = false;
+        }
+
+        if (!password) {
+            setPasswordError("Password cannot be empty.");
+            valid = false;
+        } else if (password !== "12345678") {
+            setPasswordError("Incorrect password.");
+            valid = false;
+        }
+
+        if (valid) {
+            alert("Login successful!");
+            setEmail("");
+            setPassword("");
+        }
+    };
+
     return (
-        <div className="w-full h-screen flex flex-col md:flex-row -mt-16">
-            <div className="relative w-full md:w-1/2 h-64 md:h-full overflow-hidden">
+        <div className="w-full min-h-screen flex flex-col md:flex-row -mt-16">
+
+            <div className="relative w-full md:w-1/2 h-64 md:h-auto overflow-hidden">
                 <img
                     src="/assets/Gambar2.jpg"
                     alt="Signin Image"
@@ -21,62 +57,66 @@ const SigninPage = () => {
                 />
             </div>
 
-            <div className="w-full md:w-1/2 h-full flex items-center justify-center p-4">
-                <div className="w-full max-w-md space-y-6 mt-[-60]">
+            <div className="w-full md:w-1/2 h-auto flex items-center justify-center p-4">
+                <div className="w-[550px] space-y-6 mt-[-80px]">
                     <h1 className="text-3xl font-bold text-[#333333] text-center">Sign In Form</h1>
 
-                    <form className="border-2 border-[#03649F] rounded-[10px] p-6 space-y-5 fi bg-white ml-[-50] w-[550px] h-[420px] shadow-md">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="border-2 border-[#03649F] rounded-[10px] p-6 space-y-4 bg-white shadow-md"
+                    >
 
-                        <div className="space-y-2">
+                        <div className="space-y-1">
                             <label className="text-lg font-semibold text-[#333333] flex items-center">
                                 Email <span className="text-red-500 ml-1">*</span>
                             </label>
                             <input
                                 type="email"
                                 placeholder="Enter your email in the correct format"
-                                className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] shadow-md"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className={`w-full border rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] shadow-md ${
+                                    emailError ? "border-red-500" : "border-[#03649F]"
+                                }`}
                             />
+                            <p className={`text-xs ${emailError ? "text-red-500" : "text-transparent"} min-h-[16px]`}>
+                                {emailError || "."}
+                            </p>
                         </div>
 
-                        <div className="space-y-2">
-                            <label className="text-lg font-semibold text-[#333333] flex items-center">
-                                Password <span className="text-red-500 ml-1">*</span>
-                            </label>
-                            <div className="relative">
-                                <input
-                                    type={showPassword ? "text" : "password"}
-                                    placeholder="Enter your correct password"
-                                    className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] pr-12 shadow-md"
-                                />
-                                <div
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
-                                >
-                                    {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
-                                </div>
+                        <div className="space-y-1">
+                        <label className="text-lg font-semibold text-[#333333] flex items-center">
+                            Password <span className="text-red-500 ml-1">*</span>
+                        </label>
+                        <div className="relative">
+                            <input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Enter your correct password"
+                            className={`w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] pr-12 shadow-md ${
+                                passwordError ? "border-red-500" : ""
+                            }`}
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <div
+                            onClick={togglePasswordVisibility}
+                            className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
+                            >
+                            {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
                             </div>
                         </div>
 
-                        <div className="space-y-2">
-                            <label className="text-lg font-semibold text-[#333333] flex items-center">
-                                Confirm Password <span className="text-red-500 ml-1">*</span>
-                            </label>
-                            <div className="relative">
-                                <input
-                                    type={showPassword ? "text" : "password"}
-                                    placeholder="Enter your correct password"
-                                    className="w-full border border-[#03649F] rounded-[10px] px-4 py-2 focus:outline-none placeholder:text-[#D9D9D9] placeholder:text-[14px] pr-12 shadow-md"
-                                />
-                                <div
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute inset-y-0 right-3 flex items-center cursor-pointer text-gray-500"
-                                >
-                                    {showPassword ? <FaEye size={20} /> : <FaEyeSlash size={20} />}
-                                </div>
-                            </div>
+                        {passwordError && <p className={`text-xs ${emailError ? "text-red-500" : "text-transparent"} min-h-[16px]`}>{passwordError}</p>}
+
+                        <div className="flex justify-end pt-4">
+                            <Link href="../auth/change-password" className="text-sm text-[#ED7117] font-semibold hover:underline">
+                            Forgot Password?
+                            </Link>
+                        </div>
                         </div>
 
-                        <div className="flex justify-center pt-12">
+
+                        <div className="flex justify-center pt-16">
                             <button
                                 type="submit"
                                 className="bg-[#ED7117] hover:bg-orange-600 text-white font-semibold rounded-[10px] w-[180px] h-[36px] transition"
@@ -87,7 +127,10 @@ const SigninPage = () => {
 
                         <p className="text-gray-500 text-center text-sm">
                             Don't have an account?{" "}
-                            <Link href="../auth/register" className="text-[#ED7117] font-semibold hover:underline">
+                            <Link
+                                href="../auth/register"
+                                className="text-[#ED7117] font-semibold hover:underline"
+                            >
                                 Register
                             </Link>
                         </p>
