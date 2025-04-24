6a993e63a4993e5db91f5b256dcab752fa81ebca
FathimatusZN <fathimatus.zn18@gmail.com>
2025-04-24 14:32:40 +0700

    feat(tailwind): add typography plugin for rich text styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -82,5 +82,8 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+	require('tailwindcss-animate'),
+	require('@tailwindcss/typography')
+  ],
 } satisfies Config;
