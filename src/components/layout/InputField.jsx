32c1c192bc9f91635bead423a7755c98f16104e2
FathimatusZN <fathimatus.zn18@gmail.com>
2025-05-20 11:46:44 +0700

    fix(component): fix duplicate export for discard button

--- src/components/layout/InputField.jsx
+++ src/components/layout/InputField.jsx
@@ -29,7 +29,7 @@ const SaveButton = ({ onClick, disabled = false }) => {
 };
 
 // Discard Button Component
-export const DiscardButton = ({ onClick }) => {
+const DiscardButton = ({ onClick }) => {
     return (
         <button
             className="flex items-center gap-2 bg-neutral2 text-neutral3 px-2 py-1 sm:px-3 sm:py-1.5 rounded-lg shadow hover:bg-error1 hover:text-white"
