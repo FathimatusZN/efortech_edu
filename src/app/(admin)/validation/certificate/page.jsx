a8597cfd79d4805ed07b69969ccc0381519dc382
FathimatusZN <fathimatus.zn18@gmail.com>
2025-05-23 13:54:02 +0700

    feat(validation): add loading spinner

--- src/app/(admin)/validation/certificate/page.jsx
+++ src/app/(admin)/validation/certificate/page.jsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import ValidationCertificateClient from "./ValidationCertificateClient";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 export default function ValidationCertificatePage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="items-center justify-center"><LoadingSpinner /></div>}>
       <ValidationCertificateClient />
     </Suspense>
   );
--- src/app/(admin)/validation/training/page.jsx
+++ src/app/(admin)/validation/training/page.jsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import ValidationTrainingClient from "./ValidationTrainingClient";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 export default function ValidationTrainingPage() {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<div className="items-center justify-center"><LoadingSpinner /></div>}>
             <ValidationTrainingClient />
         </Suspense>
     );
