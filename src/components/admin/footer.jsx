31ab02eb80c596ea9a6a3c2b29120311a91fc008
FathimatusZN <fathimatus.zn18@gmail.com>
2025-06-09 13:43:43 +0700

    layout(footer-admin): fix footer layout

--- src/components/admin/footer.jsx
+++ src/components/admin/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const AdminFooter = () => {
     return (
-        <footer className="bg-[#157AB2] min-h-[50px] w-full flex items-center justify-center mt-[30px] px-4">
+        <footer className="bg-[#157AB2] min-h-[50px] w-full flex items-center justify-center px-4">
             <p className="text-white text-center font-montserrat text-sm sm:text-base md:text-md lg:text-lg">
                 © 2025 Efortech Education. All Rights Reserved
             </p>
